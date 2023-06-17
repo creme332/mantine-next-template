2cba46737b819d01cec8c88e93c3cacb038a6a5a
creme332 <65414576+creme332@users.noreply.github.com>
2023-06-17 13:16:15 +0400

    minor update

--- src/pages/game.js
+++ src/pages/game.js
@@ -89,9 +89,9 @@ export default function Game() {
       <Container>
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </Container>
-      <div className={styles.gameInfo}>
+      <Container className={styles.gameInfo}>
         <ol>{moves}</ol>
-      </div>
+      </Container>
     </Container>
   );
 }
