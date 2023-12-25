c41de011338d907bdef7d7289e0a447b71de8612
creme332 <65414576+creme332@users.noreply.github.com>
2023-12-25 13:58:49 +0400

    refresh look of footer

--- components/FooterSimple.tsx
+++ components/FooterSimple.tsx
@@ -1,10 +1,13 @@
-import { Container, Group, Anchor } from "@mantine/core";
+import { Container, Group, Anchor, Text } from "@mantine/core";
 import classes from "../styles/FooterSimple.module.css";
+import React, { ReactComponentElement, ReactNode } from "react";
 
-const links = [{ link: "#", label: "Github" }];
+const links = [
+  { link: "https://github.com/creme332", label: "Github" },
+  { link: "mailto:c34560814@gmail.com", label: "Contact" },
+];
 
-//TODO: Change any to tabler-icon
-export default function FooterSimple({ icon = null }: any) {
+export default function FooterSimple({ icon }: { icon: ReactNode }) {
   const items = links.map((link) => (
     <Anchor<"a">
       c="dimmed"
@@ -21,6 +24,10 @@ export default function FooterSimple({ icon = null }: any) {
     <div className={classes.footer}>
       <Container className={classes.inner}>
         {icon}
+
+        <Text c="dimmed" size="sm">
+          Made by creme332
+        </Text>
 
         <Group className={classes.links}>{items}</Group>
       </Container>
